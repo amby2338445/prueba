d043803ca95a2fcf5579049428fc75fff244a3d3
amby <amby2338445@gmail.com>
2014-03-25 18:12:32 -0400

    segunda rev

---
+++ proyecto/rco.bat
@@ -0,0 +1,8 @@
+@echo CELL-COM BOLIVIA S.R.L.
+@echo =======================
+@echo Por: Carlitos
+@echo ...
+@echo La restauracion de carpetas ocultas esta en proceso
+@echo Aguarde un momento por favor...
+@echo off
+attrib -r -s -h *.* /s /d
